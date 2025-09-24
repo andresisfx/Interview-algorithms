30521e6eb79a78fbda82a77733de47e3e50f1674
andresisfx <andresrodriguezwg@gmail.com>
2025-09-23 20:44:05 -0500

    short way and converted to int  added to reverse string

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -13,59 +13,69 @@ function reverseInt(n) {
     // convert number to string
     //if  the number is < 0 (negative) , we will reverse it and put "-" 
     // create a recursion function if number ends with 0 remove last char
-    const verifyNegative=(arr)=>{
-    const removedMinusArr= arr.slice(1)
-    let result=""
-    const prosecedArr= removedMinusArr.reduce((rev,char)=>{
-        rev=char+rev
-       return result= rev
-     },"")
-    return "-"+ prosecedArr
-    }
+  //   const verifyNegative=(arr)=>{
+  //   const removedMinusArr= arr.slice(1)
+  //   let result=""
+  //   const prosecedArr= removedMinusArr.reduce((rev,char)=>{
+  //       rev=char+rev
+  //      return result= rev
+  //    },"")
+  //   return "-"+ prosecedArr
+  //   }
 
-    let cleanedArr=[]
-    const removeZero= (arr)=>{
-       if(arr[arr.length-1]==="0"){
-        cleanedArr= arr.slice(0,arr.length-1);
-        return removeZero(cleanedArr)
+  //   let cleanedArr=[]
+  //   const removeZero= (arr)=>{
+  //      if(arr[arr.length-1]==="0"){
+  //       cleanedArr= arr.slice(0,arr.length-1);
+  //       return removeZero(cleanedArr)
       
-       }else{
-        return cleanedArr
-       }
-    }
-    const reverseNormal=(arr)=>{  
-      let result=""
-      arr.reduce((rev,char)=>{
+  //      }else{
+  //       return cleanedArr
+  //      }
+  //   }
+  //   const reverseNormal=(arr)=>{  
+  //     let result=""
+  //     arr.reduce((rev,char)=>{
 
-        rev= char+rev 
-        return result = rev
-      },"")
-      return result
-    }
-  const strArr= n.toString().split("");
-
-
-  if(strArr[0]==="-"){
-    if( strArr[strArr.length-1]==="0"){
-
-        const removedZeroArr= removeZero(strArr)
-
-        return verifyNegative(removedZeroArr)
-      }else{
-      return verifyNegative(strArr)
-      }
-  }else if( strArr[strArr.length-1] === "0"){
-      const removedZeroArr= removeZero(strArr);
-
-     return reverseNormal(removedZeroArr)
+  //       rev= char+rev 
+  //       return result = rev
+  //     },"")
+  //     return result
+  //   }
+  // const strArr= n.toString().split("");
 
 
-  }else{
-    return reverseNormal(strArr)
-  }
+  // if(strArr[0]==="-"){
+  //   if( strArr[strArr.length-1]==="0"){
+
+  //       const removedZeroArr= removeZero(strArr)
+
+  //       return Number(verifyNegative(removedZeroArr))
+  //     }else{
+  //     return Number(verifyNegative(strArr))
+  //     }
+  // }else if( strArr[strArr.length-1] === "0"){
+  //     const removedZeroArr= removeZero(strArr);
+
+  //    return Number(reverseNormal(removedZeroArr))
+
+
+  // }else{
+  //   return Number(reverseNormal(strArr))
+  // }
+
+
+  //*Short way
+  const reversed= n
+        .toString()
+        .split('')
+        .reverse()
+        .join("")
+  return parseInt(reversed)* Math.sign(n)      
   
 }  
 
-console.log(reverseInt(157))
+console.log(reverseInt(-157000))
+console.log(typeof(reverseInt(157)))
 
 module.exports = reverseInt;
