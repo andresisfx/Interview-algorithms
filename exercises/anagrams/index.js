e9bbdb5edb92b7ab6b672783650c8aeb3f2832fb
andresisfx <andresrodriguezwg@gmail.com>
2025-10-03 21:23:34 -0500

    Anagrams solved ans test passed

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,53 +9,30 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
+  const aCharMap=buildCharMap(stringA)
+  const bCharMap=buildCharMap(stringB)
+   
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length ){
+    return false
+  }
 
-    const cleanedStr1=stringA.replace(/[^a-zA-Z0-9 ]/g,"").replace(/\s+/g,"").toLowerCase();
-    const cleanedStr2=stringB.replace(/[^a-zA-Z0-9 ]/g,"").replace(/\s+/g,"").toLowerCase();
-     if(cleanedStr1===cleanedStr2){
-        return false
-     }
-    const mapA= new Map();
-    const mapB= new Map();
-    
-    for(let char of cleanedStr1){
-      
-          if(!mapA.get(char)){
-
-              mapA.set(char,1)
-          }else{
-              mapA.set(char,mapA.get(char)+1)
-
-          }
-
-    }
-    for(let char of cleanedStr2){
-       
-          if(!mapB.get(char)){
-
-              mapB.set(char,1)
-          }else{
-              mapB.set(char,mapB.get(char)+1)
-
-          }
-
-    }
-    console.log(mapA)
-    console.log(mapB)
-    
-    for(let [key,value] of mapA.entries()){
-       
-       if(mapA.size!==mapB.size){
-        return false
-       } 
-       if(mapB.get(key)||mapA.get(key)!==value){
-        return false
-       }
-    }
-    return true
+  for(let char in aCharMap){
+      if( bCharMap[char]!==aCharMap[char]){
+        return  false
+      }
+  }
+  return true
 }
-console.log(anagrams('rail safety', 'fairy tales')) 
-console.log( anagrams('RAIL! SAFETY!', 'fairy tales')) 
-console.log(anagrams('Hi there', 'Bye there')) 
-console.log(anagrams('hello', '')) 
+function buildCharMap(str){
+    const mapChar ={};
+
+    for(let char of str.replace(/[^\w]/g).toLowerCase()){  // Alt + 94 para escribir "^""
+        mapChar[char]=mapChar[char] + 1 ||1
+    }
+    return mapChar
+}    
+console.log("1",anagrams('rail safety', 'fairy tales')) 
+console.log( "2",anagrams('RAIL! SAFETY!', 'fairy tales')) 
+console.log("3",anagrams('Hi there', 'Bye there')) 
+console.log("4",anagrams('hello', '')) 
 module.exports = anagrams;
