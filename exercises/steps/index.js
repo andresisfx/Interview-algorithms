e9a50543fc942c2849dac44d84e255654c05da0b
andresisfx <andresrodriguezwg@gmail.com>
2025-10-14 21:16:20 -0500

    steps problem recursion solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,20 +17,60 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    let str=""
-    for(let i = 0;i<n;i++){
-        str=str+" ";
-      for(let j =0;j<=i;j++){
-        str= str+"#";
-      }
-      for(let k =n-1;k>i;k--){
-        str= str+"-";
-      }
-      str= str + "\n"
-    }
-    return str
+// function steps(n) {
+  //* this solution doesnt pass the test
+    // let str=""
+    // for(let i = 0;i<n;i++){
+    //     str=str+" ";
+    //   for(let j =0;j<=i;j++){
+    //     str= str+"#";
+    //   }
+    //   for(let k =n-1;k>i;k--){
+    //     str= str+"-";
+    //   }
+    //   str= str + "\n"
+    // }
+    // console.log(str)
+
+    //*second solution
+    // for (let row = 0; row < n; row++) {
+      
+    //   let stair=''
+    //   for (let column = 0; column < n; column++) {
+    //     if(column<=row){
+    //       stair+="#"
+    //     }else{
+    //       stair+="-"
+    //     }
+        
+    //   }
+    //   console.log(stair)
+    // }
+  
    
+
+// }
+
+
+//*recurtion solution
+function steps(n,row=0,stair="") {
+  if(n===row){
+    return
+  }
+
+  if(n===stair.length){
+    console.log(stair)
+    return steps(n,row + 1)
+  }
+  
+  if(stair.length <= row){
+    stair  += "#"
+  }else{
+    stair += " "
+  }
+
+  steps(n,row,stair)
+
 }
-console.log(steps(3))
+steps(3)
 module.exports = steps;
