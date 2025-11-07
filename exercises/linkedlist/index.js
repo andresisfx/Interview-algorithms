99d2308259512abf7ecd241f7d9272ea7bac7399
andresisfx <andresrodriguezwg@gmail.com>
2025-11-06 23:14:47 -0500

    insertFisrt and size solved (linkedlist)

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,43 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class LinkedList {
+    
+    constructor(){
 
-class LinkedList {}
+        this.head= null;
+        this.tail=null;
+        this.length=0;
+    }
+
+    insertFirst(val){
+        const newNode= new Node(val);
+
+        let current= this.head;
+        if(!current){
+            this.head=newNode;
+            this.length ++
+            return;
+        }
+        
+        while (current.next) {
+            current=current.next;
+        }
+        this.head= newNode;
+        this.length =this.length + 1;
+        
+    }
+    size(){
+        return this.length
+    }
+
+}
+class Node {
+    constructor(val, next=null){
+       this.data=val;
+       this.next=next;
+    }
+    
+}
 
 module.exports = { Node, LinkedList };
