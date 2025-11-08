7a1455ee3dac4f6164f00127fe56e8c3d6cbcb3f
andresisfx <andresrodriguezwg@gmail.com>
2025-11-07 23:16:58 -0500

    insertAt linked list method solved

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -21,15 +21,146 @@ class LinkedList {
             return;
         }
         
-        while (current.next) {
-            current=current.next;
-        }
+        
         this.head= newNode;
-        this.length =this.length + 1;
+        this.head.next=current;
+        this.length ++;
         
     }
     size(){
         return this.length
+    }
+    getFirst(){
+        return this.head
+    }
+    getLast(){
+        let current = this.head;
+        while (current.next) {
+            current= current.next
+        }
+        return current
+    }
+    clear(){
+        this.head= null;
+        this.length=0;
+    }
+    removeFirst(){
+        if(!this.head)return
+        let current= this.head;
+        this.head=current.next;
+        this.length -- ;
+    }
+    removeLast(){
+        let current= this.head;
+        if(!this.head){
+            return
+        }
+        if(!current.next){
+            this.head = null;
+            this.length --;
+            return
+        }
+        
+        while (current.next.next) {
+            current= current.next;
+        }
+        current.next= null;
+        this.length --;
+
+    }
+    insertLast(data){
+      let current = this.head;
+      const newNode= new Node(data)
+      if(!this.head){
+        this.head=newNode
+        return
+      }
+      while (current) {
+        if(!current.next){
+            break;
+        }
+        current= current.next;
+      }
+      current.next =newNode;
+      this.length++;
+
+    }
+    getAt(nodeNumber){
+      let current= this.head;
+      if(!this.head){
+        return null
+      } 
+
+      let counter=0;//* in my opinion this value should start in 1 but tests are done fo this to be 0 
+     
+      while(current){
+
+          if(nodeNumber===counter){
+              
+             return current
+            }
+          current= current.next
+          counter ++;
+      }
+      return null
+      
+     
+
+    }
+    removeAt(index){
+       let current= this.head;
+       let counter = 0;
+       if(!this.head)return
+       if(index===0){
+        this.head=current.next
+        return
+       }
+       
+       while(current){
+        if (!current.next) {
+            return
+        }
+          if(index === counter+1){
+              current.next= current.next.next;
+          }
+         counter++ 
+         current= current.next;
+       }
+       
+    }
+    insertAt(value,index){
+      let current= this.head;
+      const newNode= new Node(value)
+      let counter=0;
+      if(!current){
+        this.head=newNode;
+        this.length++
+        return
+      }
+      if (index===0) {
+        this.head=newNode;
+        this.head.next=current;
+        return
+      }
+      
+      while(current){
+         if(!current.next){
+            current.next=newNode;
+            this.length++;
+            return
+         }
+         if(index === counter+1){
+           let aux= current.next;
+           current.next=newNode,
+           current.next.next=aux;
+
+            this.length++;
+            return
+         }
+         current= current.next;
+         counter++;
+
+      }
     }
 
 }
