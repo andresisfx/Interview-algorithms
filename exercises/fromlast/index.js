fa26344470db6147201f837bea8281c73c7b1d33
andresisfx <andresrodriguezwg@gmail.com>
2025-11-25 20:32:38 -0500

    fromLast problem solved

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,39 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    // *first solution
+    // let count=0
+    // let current=list.head;
+    // while (current.next) {
+    //     current= current.next;
+    //     count++
+    // }
+    // return list.getAt(count-n)
+    
+    //* second solution
+    // let slow=list.getFirst();
+    // let fast= list.getAt(n+1)
+    // while(fast.next){
+    //     fast= fast.next;
+    //     slow=slow.next;
+    // }
+    // return slow;
+    //* third solution
+
+    let fast = list.getFirst();
+    let slow = list.getFirst()
+
+    while(n > 0){
+        fast = fast.next;
+        n--
+    }
+    while(fast.next){
+        fast=fast.next;
+        slow=slow.next;
+    }
+    return slow
+    
+}
 
 module.exports = fromLast;
